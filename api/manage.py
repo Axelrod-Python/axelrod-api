02f26f4db0dd162625431db941ba6a0b3bfd7922
Owen Campbell <owen.campbell@empiria.co.uk>
2016-08-01 17:04:37 +0100

    Replace wait_for_postgres.sh with python code in manage.py

--- api/manage.py
+++ api/manage.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 import sys
+from psycopg2 import connect, OperationalError
+from time import sleep
+
+
+def database_available():
+    try:
+        connect("host=db dbname=postgres user=postgres")
+        return True
+    except OperationalError:
+        return False
 
 if __name__ == "__main__":
+    while not database_available():
+        print('Database is unavailable - Sleeping')
+        sleep(2)
+
+    print('Database is available - Starting Django Server')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
