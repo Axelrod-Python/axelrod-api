64a827cc93498bebf8b4165c3acbce0c3b62f719
Owen Campbell <owen.campbell@empiria.co.uk>
2016-08-02 14:14:26 +0100

    Disaple app specific static files

--- api/config/settings.py
+++ api/config/settings.py
@@ -121,6 +121,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    # 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
