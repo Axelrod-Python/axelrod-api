217e9bb5456847858bfaaa1a83a7acc74e9b777c
Owen Campbell <owen.campbell@empiria.co.uk>
2016-08-02 13:51:12 +0100

    Add full definition to import in urls.py

--- api/config/urls.py
+++ api/config/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
-from core.views import StrategyViewSet
+from api.core.views import StrategyViewSet
 
 
 urlpatterns = [
