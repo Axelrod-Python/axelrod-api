dd648de6960704fbf4422d0d2e8476c01f0af33f
Owen Campbell <owen.campbell@empiria.co.uk>
2016-08-09 15:36:05 +0100

    Add retrieve method

--- api/core/views.py
+++ api/core/views.py
@@ -3,10 +3,18 @@ from rest_framework.response import Response
 import axelrod as axl
 
 
+def id_from_name(name):
+    return name.lower().replace(' ', '')
+
+
 class StrategySerializer(serializers.Serializer):
+    id = serializers.SerializerMethodField()
     name = serializers.CharField(max_length=200)
     description = serializers.SerializerMethodField()
     classifier = serializers.DictField()
+
+    def get_id(self, strategy):
+        return id_from_name(strategy.name)
 
     def get_description(self, strategy):
         return strategy.__doc__
@@ -16,4 +24,11 @@ class StrategyViewSet(viewsets.ViewSet):
 
     def list(self, request):
         serializer = StrategySerializer(axl.strategies, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        strategy_dict = {
+            id_from_name(strategy.name): strategy for strategy in axl.strategies}
+        strategy = strategy_dict[id_from_name(pk)]
+        serializer = StrategySerializer(strategy)
         return Response(serializer.data)
