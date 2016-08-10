540b404b2c109b9ae0bfd2f50981ddd68e4d9819
Owen Campbell <owen.campbell@empiria.co.uk>
2016-08-10 10:24:03 +0100

    Move strategies dict to class variable

--- api/core/views.py
+++ api/core/views.py
@@ -6,14 +6,14 @@ from api.core.serializers import StrategySerializer, strategy_id
 
 class StrategyViewSet(viewsets.ViewSet):
 
+    strategies = {strategy_id(s): s for s in axl.strategies}
+
     def list(self, request):
         serializer = StrategySerializer(
             axl.strategies, many=True, context={'request': request})
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        strategy_dict = {
-            strategy_id(s): s for s in axl.strategies}
-        strategy = strategy_dict[pk]
+        strategy = self.strategies[pk]
         serializer = StrategySerializer(strategy, context={'request': request})
         return Response(serializer.data)
